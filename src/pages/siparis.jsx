353b69358364be1aa89624af70f49454611dd245
Emre <emrealtinoz210@gmail.com>
2024-12-03 22:37:54 +0300

    f-commit

---
+++ src/pages/siparis.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function siparis() {
+  return (
+    <div>siparis</div>
+  )
+}
+
+export default siparis
---
+++ src/pages/sonuc.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function sonuc() {
+  return (
+    <div>sonuc</div>
+  )
+}
+
+export default sonuc
